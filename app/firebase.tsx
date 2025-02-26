2ccb8f4cb6de2e4ee9b51cde5e5f01922ab00d6d
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-02-26 15:04:08 +0200

    Basic products in Firebase

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+import { Paragraph, Screen, Title } from "@/utils/components";
+import { styles } from "@/utils/styles";
+import { useState } from "react";
+import { Button, FlatList, Text, TextInput, View } from "react-native";
+
+type Product = { title: string, amount: string };
 
 export default function Index() {
+
+  const [product, setProduct] = useState({ title: '', amount: '' });
+  const [items, setItems] = useState<Product[]>([]);
+
+  const handleSave = () => {
+    setItems([...items, product]);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Firebase</Text>
-    </View>
+    <Screen>
+      <Title>Firebase</Title>
+      <Paragraph>
+        Firebase is a platform developed by Google that provides a variety of tools and services for building and managing mobile and web applications.
+      </Paragraph>
+      <Paragraph>
+        Firebase provides Realtime Database that is NoSQL cloud database. Data is stored as JSON and synchronized in realtime to every connected client.
+      </Paragraph>
+
+      <TextInput
+        placeholder='Product title'
+        style={styles.textInput}
+        onChangeText={text => setProduct({ ...product, title: text })}
+        value={product.title} />
+
+      <TextInput
+        placeholder='Amount'
+        style={styles.textInput}
+        onChangeText={text => setProduct({ ...product, amount: text })}
+        value={product.amount} />
+
+      <Button onPress={handleSave} title="Save" />
+
+      <FlatList
+        renderItem={({ item }) =>
+          <Text style={{ fontSize: 18 }}>{item.title}, {item.amount}</Text>
+        }
+        data={items} />
+    </Screen >
   );
 }
